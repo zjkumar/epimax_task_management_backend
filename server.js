535c0c29fd92e84acd80f6485b256c7fb8fa31c7
zjkumar <zjagadeeshkumar@gmail.com>
2024-05-04 20:00:57 +0530

    updated server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "OPTIONS, DELETE, POST, GET, PATCH, PUT");
   // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, section_id, Origin, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 })
 
