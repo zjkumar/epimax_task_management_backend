b62c23917b314860b0454858f8731d53a5b41f0c
zjkumar <139661058+zjkumar@users.noreply.github.com>
2024-05-04 11:25:57 +0530

    Updated configurations to server.js

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(bodyParser.json());
 // using cors
 // app.use(cors())
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
 // Create MySQL connection pool
 const pool = mysql.createPool({
 
