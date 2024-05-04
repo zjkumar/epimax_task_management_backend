4e755f1cb54fdaa32bd92114d5620d7e8a4c4904
zjkumar <139661058+zjkumar@users.noreply.github.com>
2024-05-04 11:30:57 +0530

    Updated configurations to server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, DELETE, POST, GET, PATCH, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
